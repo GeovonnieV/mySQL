4e05ae8fc5ae4a984e4be3f2e85db6d899bae4a5
geovonnie vasquez <geovonnievasquez@gmail.com>
2020-03-18 15:27:43 -0500

    department view and add

--- index.js
+++ index.js
@@ -7,10 +7,93 @@ const connection = mysql.createConnection({
     port: 3306,
     password: "Makeitea$y8",
     user: "root",
-    // database:
+    database: "sql_hw"
 
 })
 //connecting
 connection.connect(function(err){
     if(err) throw err
+    start()
 })
+
+//function that holds logic
+function start(){
+//letting user choose the method they want to run
+    inquirer.prompt([
+        {
+            name: 'options',
+            type: 'list',
+            message:'will you like to do?',
+            choices:[
+                "add department",
+                "add roles",
+                "add employees",
+                "view department",
+                "view roles",
+                "view employees ",
+                "update employee roles"
+            ]
+
+        }
+        
+
+    ]).then(function(answers){
+        switch (answers.options) {
+            case "add department": addDepartment()
+                
+                break;
+            case "view department": viewDepartment()
+                
+                break;
+
+            default:
+                break;
+        }
+    })
+
+
+
+
+}
+//adds the department 
+function addDepartment(){
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "departmentName",
+            message: "what is the departments name?"
+        },
+        {
+            type: "number",
+            name: "depId",
+            message: "what is the departments id"
+        }
+
+    ]).then(function(answer){
+        const query = "Insert Into department SET ?"
+        connection.query(query,{
+          name:answer.departmentName,
+          id:answer.depId
+        },
+        function(err){
+            if(err) throw err
+            console.log("Department added")
+            start()
+        })
+    })
+
+
+
+};
+
+//view department
+function viewDepartment(){
+    const query = "SELECT * FROM department"
+    connection.query(query, function(err,res){
+        if(err) throw err;
+        for(var i = 0; i < res.length; i++){
+            console.log(res[i].id + "-" + res[i].name)
+        }
+        start()
+    })
+}
--- seed.sql
+++ seed.sql
@@ -1,10 +1,34 @@
+DROP DATABASE IF EXISTS sql_hw;
+CREATE DATABASE sql_hw;
 
+use sql_hw;
 
 create table employees
-values (
+(
+    id int NOT NULL AUTO_INCREMENT,
     first_name varchar(30),
     last_name varchar(30),
-    role_id init,
-    manager_id init
-)
+    role_id int,
+    manager_id int NULL,
+    PRIMARY KEY (id)
+);
 
+
+
+create table roles 
+(
+    id int NOT NULL AUTO_INCREMENT,
+    title varchar(30),
+    salary decimal(100000,4),
+    department_id int,
+    PRIMARY KEY (id)
+);
+
+
+
+create table department
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    name varchar(30),
+    PRIMARY KEY (id)
+);
